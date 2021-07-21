6aa36d62faf8443248751ac690846b60ce1f6386
jeromrBor <jerome.borga@gmail.com>
2021-07-21 17:32:35 +0200

    started to make filter, what a mess!

--- src/components/DrawingsList.jsx
+++ src/components/DrawingsList.jsx
@@ -1,25 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import fetchDrawings from '../utils/fetchDrawings';
 import fetchTags from '../utils/fetchTags';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 import '../styles/drawinglist.css';
 import DrawingCard from './DrawingCard';
 
 const DrawingsList = () => {
   const [drawings, setDrawings] = useState();
+  const [filteredDrawings, setFilteredDrawings] = useState(drawings);
   const [tags, setTags] = useState();
 
+  const filter1 = drawings && drawings.filter((e) => e.tagsId === 1);
+
+  function removeFiter() {
+    setFilteredDrawings(drawings);
+  }
+
   useEffect(() => {
-    fetchTags(setTags);
+    async function fetchApiTags() {
+      await fetchTags(setTags);
+      setTags();
+    }
+    fetchApiTags();
+    console.log(tags);
   }, []);
+
   useEffect(() => {
-    fetchDrawings(setDrawings);
+    async function fetchApiDrawings() {
+      await fetchDrawings(setDrawings);
+      setFilteredDrawings(drawings);
+    }
+    fetchApiDrawings();
+    console.log(drawings);
+    console.log(filteredDrawings);
   }, []);
+
+  function showFilter1() {
+    setFilteredDrawings(filter1);
+  }
 
   return (
     <Container fluid className='drawingcard container my-5'>
       <h1 className='page-title fw-bold text-center'>La gallerie</h1>
+      <div className='filter-card'>
+        <span>Filter par : </span>
+        <Button onClick={() => showFilter1()}>Aquarelle</Button>
+        <Button>Promarker</Button>
+        <Button>Digital</Button>
+        <Button>Autres</Button>
+        <Button onClick={() => removeFiter()}>Remove Filter</Button>
+      </div>
       <div className='drawingcard-wrapper'>
         {drawings &&
           drawings.map((drawing) => (
