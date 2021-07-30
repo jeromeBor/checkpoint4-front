7315d084653ba734c0bd67d17b60e116daf3591f
jeromrBor <jerome.borga@gmail.com>
2021-07-30 21:22:09 +0200

    improved some css

--- src/components/DrawingCard.jsx
+++ src/components/DrawingCard.jsx
@@ -8,25 +8,23 @@ const DrawingCard = ({ filteredDrawings, tags }) => {
     <>
       {filteredDrawings &&
         filteredDrawings.map((drawing) => (
-          <Link key={drawing.id} to={`/drawing/${drawing.id}`}>
+          <div className='d-flex justify-content-center'>
             <Card key={drawing.id} className='drawing-card m-2'>
-              <div className='cardimage-overlay-text fw-bold text-secondary'>
-                <span className='cardtitle fs-2'> {drawing.title}</span>
-                <span className='cardtag fs-5 fw-light text-light'>
-                  {tags &&
-                    tags.filter((tag) => tag.idCategory === tag.idCategory)[0]
-                      .title}
-                </span>
-                {/* <span className='carddate fs-6 fw-light text-info'>
-                  {drawing.dateOfWrite && formatDateUnix(drawing.dateOfWrite)}
-                </span> */}
-              </div>
-              <div className='cardimage-overlay d-flex align-items-center justify-content-center'></div>
-              <div className='cardimage-container'>
-                <Card.Img variant='top' src={drawing.imageLink} />
-              </div>
+              <Link to={`/drawing/${drawing.id}`}>
+                <div className='cardimage-overlay-text fw-bold text-secondary'>
+                  <span className='cardtitle fs-2'> {drawing.title}</span>
+                  <span className='cardtag fs-5 fw-light text-light'>
+                    {tags &&
+                      tags.filter((tag) => tag.id === drawing.tagsId)[0].title}
+                  </span>
+                </div>
+                <div className='cardimage-overlay d-flex align-items-center justify-content-center'></div>
+                <div className='cardimage-container'>
+                  <Card.Img variant='top' src={drawing.imageLink} />
+                </div>
+              </Link>
             </Card>
-          </Link>
+          </div>
         ))}
     </>
   );
